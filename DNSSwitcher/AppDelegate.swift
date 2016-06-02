2d0141a9bc80ddf062d1c8e62534aa58e5df2423
Matt McNeeney <matt@mattmc.co.uk>
2016-06-02 12:18:47 +0100

    New project

---
+++ DNSSwitcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DNSSwitcher
+//
+//  Created by Matthew McNeeney on 02/06/2016.
+//  Copyright © 2016 mattmc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
