a6f690c0fa797e61f74e12fe6e007e4ac24fd107
Matt McNeeney <matt@mattmc.co.uk>
2016-06-02 16:14:25 +0100

    Integrate CocoaPods

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+target 'DNSSwitcher' do
+   pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
+end
+
---
+++ Pods/Target Support Files/Pods-DNSSwitcher/Pods-DNSSwitcher-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DNSSwitcher : NSObject
+@end
+@implementation PodsDummy_Pods_DNSSwitcher
+@end
